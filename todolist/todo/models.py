569bd04e6542f324d72e0f5cacf25b4e97b99797
Jonathan Villanueva <me@jevillanueva.dev>
2024-04-11 22:25:57 -0400

    add  design with bulma css framework.

--- todolist/todo/models.py
+++ todolist/todo/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Task(models.Model):
+class Task(models.Model): 
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
